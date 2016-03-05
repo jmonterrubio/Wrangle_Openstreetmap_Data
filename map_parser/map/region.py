1b2fdf556df32492900ce97cbf4d601aaa5599dc
Javier Monterrubio <j.monte.m@gmail.com>
2016-03-05 19:53:39 +0100

    Improved code of region fix

--- map_parser/map/region.py
+++ map_parser/map/region.py
@@ -6,41 +6,60 @@ Created on 01/02/2016
 '''
 
 REGION_KEY = 'is_in'
-REGION_VALUE_SEPARATOR = ':'
+REGION_VALUE_SEPARATOR = ';'
 
+def set_correct_separator(fixed_value):
+    return fixed_value.replace(',',REGION_VALUE_SEPARATOR)
+
+def set_spanish_language(value):
+    fixed_value = value.replace('Spain','España')
+    fixed_value = fixed_value.replace('Europe','Europa')
+    return fixed_value
+
+def add_unexist_entities(value):
+    fixed_value = value
+    if not 'Europa' in value:
+        fixed_value = fixed_value + ';Europa'
+    if not 'España' in fixed_value:
+        fixed_value = fixed_value + ';España'
+    return fixed_value
+
+def remove_duplicates(value):
+    value_list = value.split(REGION_VALUE_SEPARATOR)
+    final_list = []
+    for name in value_list:
+        if name in final_list: continue
+        final_list.append(name)
+    return to_string(final_list)
+
+def to_string(value_list):
+    value = ''
+    for name in value_list:
+        if value == '':
+            value = name
+        else:
+            value = value + REGION_VALUE_SEPARATOR + name
+    return value
+
+def order_entity(value_list, entity_name, entity_position):
+    unknown_name = value_list[entity_position]
+    if not unknown_name == entity_name:
+        entity_position_to_fix = value_list.index(entity_name)
+        value_list.insert(entity_position, value_list.pop(entity_position_to_fix))
+
+def order_entities(value):
+    value_list = value.split(REGION_VALUE_SEPARATOR)
+    order_entity(value_list, 'Europa', len(value_list)-1)
+    order_entity(value_list, 'España', len(value_list)-2)        
+    return to_string(value_list)
+            
 def fill(reg, key, value):
     if key == REGION_KEY:
-        fixed_value = value.replace(',',';')
-        fixed_value = fixed_value.replace('Spain','España')
-        fixed_value = fixed_value.replace('Europe','Europa')
-        if not 'Europa' in fixed_value:
-            fixed_value = fixed_value + ';Europa'
-        if not 'España' in fixed_value:
-            fixed_value = fixed_value + ';España'
-        fixed_list = fixed_value.split(';')
-        final_list = []
-        for name in fixed_list:
-            if name in final_list: continue
-            final_list.append(name)
-        
-        t_name = final_list[len(final_list)-1]
-        if not t_name == 'Europa':
-            e_pos = final_list.index('Europa')
-            final_list[e_pos] = final_list[len(final_list)-1]
-            final_list[len(final_list)-1] = 'Europa'
-            
-        t_name = final_list[len(final_list)-2]
-        if not t_name == 'España':
-            e_pos = final_list.index('España')
-            final_list[e_pos] = final_list[len(final_list)-2]
-            final_list[len(final_list)-2] = 'España'
-            
-        final_value = ''
-        for name in final_list:
-            if final_value == '':
-                final_value = name
-            else:
-                final_value = final_value + ';' + name  
-        reg[key] = final_value
+        fixed_value = set_correct_separator(value)
+        fixed_value = set_spanish_language(fixed_value)
+        fixed_value = add_unexist_entities(fixed_value)
+        fixed_value = remove_duplicates(fixed_value)
+        fixed_value = order_entities(fixed_value)
+        reg[key] = fixed_value
         return True
     return False
